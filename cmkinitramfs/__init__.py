8e7dd02f643d1c48a9001bdb5ac71ae816849c30
Louis Leseur <louis.leseur@gmail.com>
2021-12-09 08:23:12 +0100

    Bump to 0.2.2

--- cmkinitramfs/__init__.py
+++ cmkinitramfs/__init__.py
@@ -5,7 +5,7 @@ __copyright__ = 'Copyright (c) 2020 Louis Leseur'
 __email__ = 'louis.leseur@gmail.com'
 __license__ = 'MIT'
 __url__ = 'https://github.com/teapot9/fand'
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 
 import logging
 
